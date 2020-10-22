7c1356fa3f91b457ac35a60b5f45dae9fa081f81
Sandy Jacobson <sandyjacobson@Sandys-MBP.home>
2020-10-22 19:57:35 -0400

    players added to team

--- client/src/services/teams.js
+++ client/src/services/teams.js
@@ -25,7 +25,7 @@ export const destroyTeam = async (id) => {
   return resp;
 }
 
-export const addPlayer = async(teamId, playerId) => {
-  const resp = await api.put(`/players/${playerId}/teams/${teamId}`);
+export const updatePlayer = async(playerId, playerData) => {
+  const resp = await api.put(`/players/${playerId}`, { player : playerData });
   return resp.data;
 }

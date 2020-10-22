41066fa3303eee5470252608f180ffbd19e76606
Sandy Jacobson <sandyjacobson@Sandys-MBP.home>
2020-10-22 15:55:29 -0400

    teams

--- client/src/screens/Teams/TeamDetails.jsx
+++ client/src/screens/Teams/TeamDetails.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { getOneTeam} from '../../services/teams';
+import { getOneTeam } from '../../services/teams';
 
 const TeamDetails = (props) => {
   const [team, setTeam] = useState(null);

cd2fc3d5edc4f895b78ab4183dd823d25ea66163
Sandy Jacobson <sandyjacobson@Sandys-MBP.home>
2020-10-23 15:30:56 -0400

    morestyling

--- client/src/screens/Teams/TeamDetails.jsx
+++ client/src/screens/Teams/TeamDetails.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { addPlayer, getOneTeam} from '../../services/teams';
+import { addPlayer, getOneTeam } from '../../services/teams';
+import './TeamDetails.css'
 
 const TeamDetails = (props) => {
   const [team, setTeam] = useState(null);
@@ -33,13 +34,14 @@ const TeamDetails = (props) => {
       {
         team &&
         <>
+          <div className='single-team-div'>
           <div className='team-details'>
           <h3>{team.name}</h3>
             <p>{team.manager}</p>
-            </div>
           <div className="team-detail-pictures">
             <img src={team.img_url} alt={team.manager} />
-          </div>
+              </div>
+              </div>
           <h3>Players:</h3>
           {team.players.map(player => (
             <p key={player.id}>{player.name} #{player.number}</p>
@@ -53,6 +55,7 @@ const TeamDetails = (props) => {
             </select>
             <button>add</button>
           </form> */}
+            </div>
         </>
       }
     </div>
